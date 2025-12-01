0352f42d8ee5b49db82ce842712c5356326d59d6
thenam-dev <ngthenam2801@gmail.com>
2025-12-01 10:49:39 +0700

    State

--- src/views/example/MyComponent.js
+++ src/views/example/MyComponent.js
@@ -1,17 +1,44 @@
 /**
- * JSX (code html trong javascript thay vi tra 1 bien)
+ * JSX (code html trong javascript thay vi tra 1 bien) -> return block
  * How to create a component(vien gach tao nen bo cuc cua Reactjs)
  * import
  * write 1 class component
  * export
  *
+ * Fragment (group many htm element)
  */
 
 import React from "react";
 
 class MyComponent extends React.Component {
+  //key:value
+  state = {
+    name: "thenam",
+    address: "Hanoi",
+  };
+
+  handleOnChangeName = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
   render() {
-    return <div>Hello my component "Thenam-dev"</div>;
+    // let name = "thenam"; // variable in JSX
+
+    return (
+      // dung fragment thay vi boc bang div
+      <React.Fragment>
+        <div className="first">
+          <input
+            value={this.state["name"]}
+            type="text"
+            onChange={(event) => this.handleOnChangeName(event)}
+          />
+          Hello my component, My name is {this.state.name}
+        </div>
+        <div className="second">I was born in {this.state.address}</div>
+      </React.Fragment>
+    );
   }
 }
 
